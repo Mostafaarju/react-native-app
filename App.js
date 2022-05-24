c82ac691230abc69f2ba055baba7aaad5dcb3a81
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2022-05-24 15:53:38 +0200

    Add Pactise file

--- App.js
+++ App.js
@@ -1,35 +1,99 @@
-import { useState } from 'react';
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-const Separator = () => <View style={styles.separator} />;
+export const Button = ({ title }) => {
+  return (
+    <Pressable
+      style={{
+        padding: 10,
+        alignItems: 'center',
+        backgroundColor: '#61dafb',
+        marginTop: 10,
+      }}
+      onPress={() => {
+        alert('Press');
+      }}
+    >
+      <Text style={{ color: 'white' }}>{title}</Text>
+    </Pressable>
+  );
+};
 
 export default function App() {
-  const [text, setText] = useState({
-    name: 'Raju Talukder',
-    age: 28,
-    city: 'Carrara',
-  });
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text>{text.name}</Text>
-        <Text>{text.age}</Text>
-        <Text>{text.city}</Text>
-        <Separator />
-        <Button
-          onPress={() =>
-            setText({
-              name: 'HEllo',
-              age: 20,
-              city: 'Bangladesh',
-            })
-          }
-          title='Learn Mores'
-        >
-          Click Me
-        </Button>
-      </View>
-    </SafeAreaView>
+    <>
+      <StatusBar backgroundColor='#61dafb' />
+      <ScrollView>
+        <View style={{ height: 60, backgroundColor: 'cyan' }}>
+          <Text>This is a Text</Text>
+        </View>
+        <View style={{ height: 60, backgroundColor: 'turquoise' }}>
+          <Text
+            style={{
+              fontSize: 24,
+              fontWeight: 'bold',
+              color: 'white',
+              padding: 10,
+            }}
+          >
+            This is a Text
+          </Text>
+        </View>
+        <View style={{ height: 60, backgroundColor: 'aqua' }}>
+          <Text>This is a Text</Text>
+        </View>
+        <View style={{ marginTop: 50, marginLeft: 50 }}>
+          <Image
+            style={{ height: 200, width: 200 }}
+            source={require('./assets/favicon.png')}
+            // resizeMethod='cover'
+          />
+        </View>
+
+        <Button title='Press' />
+        <Button title='Press 2 Button' />
+
+        <View style={{ marginTop: 50, marginLeft: 50 }}>
+          <Image
+            style={{ height: 200, width: 200 }}
+            source={{ uri: 'https://picsum.photos/200' }}
+            resizeMode={'contain'}
+          />
+        </View>
+
+        <View style={{ marginTop: 50, marginLeft: 50 }}>
+          <Image
+            style={{ height: 200, width: 200 }}
+            source={{ uri: 'https://picsum.photos/200' }}
+            resizeMode={'contain'}
+          />
+        </View>
+
+        <View style={{ padding: 10 }}>
+          <Text style={{ padding: 10 }}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam,
+            iure. Vero, autem ea! Accusamus blanditiis quas totam officiis unde
+            eius aliquid perspiciatis repellendus sequi ab cumque distinctio
+            debitis doloremque ut id enim atque, similique possimus. Tenetur
+            optio qui mollitia explicabo neque minima debitis modi, repellendus
+            dolorem minus magnam vel quae a nihil? Nulla esse blanditiis
+            architecto magnam, consectetur dolorum eum nobis nostrum corrupti
+            unde praesentium tempore accusantium! Consectetur eaque eligendi
+            tempore eos praesentium cum eum, similique iure nemo molestiae!
+            Sequi sapiente perferendis voluptatem consequatur, ducimus itaque
+            rem facere natus debitis, libero earum praesentium voluptatibus
+            voluptates unde repellendus enim molestias sunt!
+          </Text>
+        </View>
+      </ScrollView>
+    </>
   );
 }
 
